4c35e710113f25e6eebbf30f2164a7f33b5de4c0
lvjian <lvjian@dayang.com.cn>
2013-01-22 15:05:34 +0800

    将js代码移动到src下

--- public/src/models.js
+++ public/src/models.js
@@ -39,56 +39,6 @@ var FieldModel = Backbone.Model.extend({
 });
 
 // emit event.
-var FieldView = Backbone.View.extend({
-	tagName: 'div',
-	className: 'cell',
-	template: _.template( $('#tmpl-text').html() ),
-
-	events: {
-		'change input': "onChanged"	
-	},
-	/*
-	 * name changed, 需要改变input&label的id
-	 * 改变input中的name
-	 * */
-	initialize: function() {
-		this.model.bind('change:name', this.nameUpdated, this);		
-		this.model.bind('change:value', this.valueUpdated, this);
-	},
-	render: function() {
-		var json = this.model.toJSON();
-		var html = this.template(json);
-	
-		this.$el.html(html);
-		this.cellBody =  this.$el;
-		this.label = this.$('label');
-		this.inputTag =  this.$('input');
-
-		return this;
-	},
-	nameUpdated: function() {
-		var id = this.model.get('id');
-		var name = this.model.getName();
-		this.label.attr(id);
-
-		this.inputTag.attr('id', id)
-			.attr('name', name);
-	},
-	valueUpdated:	function() {
-		var value = this.model.getValue();
-		this.inputTag.val(value);
-	},
-	onChanged: function(e) {
-		var val = this.inputTag.val();	
-
-		this.model.set({
-				value: val
-			}, {
-				silent: true
-			});
-	}
-});
-
 var SelectFieldModel = FieldModel.extend({
 	defaults: function() {
 		return {
