a60da36addcf22113244dbe03afbfacb3b4afe08
lvjian <lvjian@dayang.com.cn>
2013-01-22 10:11:19 +0800

    finish field model & view

--- public/index.js
+++ public/index.js
@@ -9,42 +9,101 @@ var FieldModel = Backbone.Model.extend({
 			required: false
 		};
 	},
+	setName: function(name) {
+		this.set({
+			id: name + '_field',
+			name: name
+		});
+	},
+	getName: function() {
+		return this.get('name');
+	},
 	setValue: function(value) {
 		this.set({value: value});	
 	},
 	getValue: function() {
-		this.get('value');
+		return this.get('value');
+	},
+	getDisplayValue: function() {
+		return this.get('value');
 	},
 	setRequired: function(required) {
 		this.set({required: required});	
 	},
 	isRequired: function() {
 		return this.get('required');
+	},
+	setRequired: function(required) {
+		this.set({required: required});
 	}
 });
-
-var field = new FieldModel;
-field.on('change:required', function(model, required) {
-	console.log('old value:' + model.previous('required'));
-	console.log('new value:' + required);
-});
-
-field.setRequired(true);
 
 // emit event.
 var FieldView = Backbone.View.extend({
 	tagName: 'div',
 	className: 'cell',
-	event: {
+	template: _.template( $('#tmpl-text').html() ),
+
+	events: {
+		'change input': "onChanged"	
 	},
-	
+	/*
+	 * name changed, 需要改变input&label的id
+	 * 改变input中的name
+	 * */
 	initialize: function() {
+		this.model.bind('change:name', this.nameUpdated, this);		
+		this.model.bind('change:value', this.valueUpdated, this);
 	},
 	render: function() {
+		var json = this.model.toJSON();
+		var html = this.template(json);
+	
+		this.$el.html(html);
+		this.cellBody =  this.$el;
+		this.label = this.$('label');
+		this.inputTag =  this.$('input');
+
+		return this;
+	},
+	nameUpdated: function() {
+		var id = this.model.get('id');
+		var name = this.model.getName();
+		this.label.attr(id);
+
+		this.inputTag.attr('id', id)
+			.attr('name', name);
+	},
+	valueUpdated:	function() {
+		var value = this.model.getValue();
+		this.inputTag.val(value);
+	},
+	onChanged: function(e) {
+		var val = this.inputTag.val();	
+
+		this.model.set({
+				value: val
+			}, {
+				silent: true
+			});
 	}
 });
 
-
+var SelectFieldModel = FieldModel.extend({
+	defaults: function() {
+		return {
+			id: '',
+			name: '',
+			label: '',
+			type: 'select',
+			value: {
+				val: '',
+				text: ''
+			},// 可能是object
+			required: false
+		};
+	}
+});
 
 var ColumnModel = Backbone.Model.extend({ 
 	
@@ -74,5 +133,4 @@ var FormView = Backbone.View.extend({
 	
 });
 
-$(function() {
-});
+
--- public/spec/IndexSpec.js
+++ public/spec/IndexSpec.js
@@ -1,8 +1,58 @@
-describe("My first Test for FormDesigner", function() {
-	var field = new FieldModel;
+describe("FieldModel & View Testing", function() {
 
-	it("ok",	function() {
-		expect('text').toEqual(
-			field.get('type') );
+	var field = false;
+	
+	beforeEach(function() {
+		field = new FieldModel();
 	});
+
+	afterEach(function() {
+		field = false;
+	});
+
+	it("Default value must be {type: \'text\', required: false}",
+		function() {
+			console.log(typeof field.constructor);
+			expect(field.get('type'))
+				.toEqual('text');
+
+			expect(field.isRequired())
+				.toEqual(false);
+		});
+
+	it("setName() should change id, id should be ${name}_field.",
+		function() {
+			field.setName('fieldName');
+
+			expect('fieldName').toEqual(
+				field.getName() 
+			);
+
+			expect('fieldName_field').toEqual(
+				field.get('id') 
+			);
+		});
 });
+
+describe("SelectModel Testing", function() {
+	var select = false;
+
+	beforeEach(function() {
+		select = new SelectFieldModel();
+	});
+
+	afterEach(function() {
+		select = false;
+	});
+
+	it("Default value must be {type:\'select', value:{val:\'\', text: \'\'}}", 
+		function() {
+			
+			expect(select.get('type'))
+				.toEqual('select')
+
+			expect(select.getValue())
+				.toEqual({val: '', text:''});
+		});
+});
+
