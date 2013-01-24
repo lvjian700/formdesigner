e6980facaaa40afab1c2e89da47f265a43539b34
lvjian <lvjian@dayang.com.cn>
2013-01-24 11:36:50 +0800

    添加ColumnModel

--- public/src/models.js
+++ public/src/models.js
@@ -57,7 +57,41 @@ var SelectFieldModel = FieldModel.extend({
 });
 
 var ColumnModel = Backbone.Model.extend({ 
-	
+	defaults: function() {
+		return {
+			index: 0,
+			colspan: 1,
+			content: {
+				id: '',
+				name: '',
+				label: '',
+				type: 'text',
+				value: '',// 可能是object
+				required: false
+			}
+		};
+	},
+	initialize: function() {
+		this.content = new FieldModel(this.content);
+		this.content.parent = this;
+
+		var json = this.content.toJSON();
+		this.set({
+			content: json
+		}, { silence: true });
+	},
+	setContent: function(fieldModel) {
+		this.content = fieldModel;
+		this.content.parent = this;
+
+		var json = this.content.toJSON();
+		this.set({
+			content: json
+		});
+	},
+	getContent: function() {
+		return this.content;
+	}
 });
 
 
