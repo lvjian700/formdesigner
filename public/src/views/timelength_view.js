bc7f39b24eedb5e0f0042e15a910653e243caa24
lvjian <lvjian@dayang.com.cn>
2013-01-31 17:35:18 +0800

    实现timelength_view.js

--- public/src/views/timelength_view.js
+++ public/src/views/timelength_view.js
@@ -2,20 +2,7 @@ var TimelengthView = FieldView.extend({
 	tagName: 'div',
 	className: 'cell input-append bootstrap-timepicker-component controls',
 	template: read_tmpl('#tmpl-timelength'),
-	render: function() {
-		var json = this.model.toJSON();
-		var html = this.template(json);
-	
-		this.$el.html(html);
-		this.cellBody = this.$el;
-		this.cellBody.addClass('bootstrap-timepicker-component');
-
-		this.label = this.$('label');
-		this.inputTag =  this.$('input');
-
-		return this;
-	},
-	onDomReady: function() {
+	onRender: function() {
 		this.inputTag.timepicker({
 			showSeconds: true,
 			showFrames: true,
