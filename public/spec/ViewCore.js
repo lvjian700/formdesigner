cccb163fd2ba64638c7229fffe017e2ff78aa0f3
lvjian <lvjian@dayang.com.cn>
2013-01-31 14:54:18 +0800

    重构core.js, 调整FieldViews对象

--- public/spec/ViewCore.js
+++ public/spec/ViewCore.js
@@ -1,30 +1,48 @@
 describe('测试views/core.js', function() {
+	
+	var Reg = null;
 
-	describe('测试Fields注册', function() {
+	beforeEach(function() {
+		Reg = new FieldViews();
+	});
 
-		it('Fields.reg跟据类型注册view class, Field.unreg可以取消注册', function() {
-			Fields.reg('text', FieldView);
-			expect(Fields.getCls('text')).toBeDefined();
-			expect(Fields.getCls('text')).toEqual(FieldView);
+	afterEach(function() {
+		Reg.clear();
+	});
 
-			Fields.unreg('text');
-			var empty = Fields.getCls('text');
+	describe('Reg基本测试', function() {
+		it('ViewCore.js默认提供一个Fields-FieldViews', function() {
+			expect(Fields).toBeDefined();
+			expect(Fields.constructor).toEqual(FieldViews);
+		});
+
+		it('FieldViews 有正确的prototype', function() {
+			expect(Reg.constructor).toEqual(FieldViews);
+			expect(Fields.constructor).toEqual(FieldViews);
+		});
+	});
+
+	describe('测试Reg注册', function() {
+
+		it('Reg.reg跟据类型注册view class, Field.unreg可以取消注册', function() {
+			Reg.reg('text', FieldView);
+			expect(Reg.getCls('text')).toBeDefined();
+			expect(Reg.getCls('text')).toEqual(FieldView);
+
+			Reg.unreg('text');
+			var empty = Reg.getCls('text');
 			expect(empty).toBeUndefined();
 		});
 
 	});
 
-	describe('测试Fields.createView', function() {
+	describe('测试Reg.create', function() {
 		beforeEach(function() {
-			Fields.reg('text', FieldView);	
-			Fields.reg('select', SelectFieldView);
+			Reg.reg('text', FieldView);	
+			Reg.reg('select', SelectFieldView);
 		});
 
-		afterEach(function() {
-			Fields.clearRegs();	
-		});
-
-		it('Fields.createView, 跟据model.type创建text field view', function() {
+		it('Reg.create, 跟据model.type创建text field view', function() {
 			var model = new FieldModel({
 				id : 'model_field',
 				name: 'model',
@@ -33,7 +51,7 @@ describe('测试views/core.js', function() {
 				required: false
 			});
 
-			var view = Fields.createView({
+			var view = Reg.create({
 				model: model
 			});
 			view = view.render();
@@ -47,7 +65,7 @@ describe('测试views/core.js', function() {
 			expect(view.inputTag[0].tagName.toLowerCase()).toEqual('input');
 		});
 
-		it('Fields.createView, 创建select view', function() {
+		it('Reg.create, 创建select view', function() {
 			
 			var model = new FieldModel({
 				id : 'model_field',
@@ -57,7 +75,7 @@ describe('测试views/core.js', function() {
 				required: false
 			});
 
-			var view = Fields.createView({
+			var view = Reg.create({
 				model: model
 			});
 			view = view.render();
--- public/src/views/core.js
+++ public/src/views/core.js
@@ -2,25 +2,28 @@ function read_tmpl (tmplSelector) {
 	return _.template( $(tmplSelector).html() )
 }
 
-var Fields =  {
-	f_types: {},
-	reg: function (fieldType, viewCls) {
-		this.f_types[fieldType] = viewCls;
-	},
-	unreg: function(fieldType) {
-		this.f_types[fieldType] = undefined;
-	},
-	getCls: function(fieldType) {
-		return this.f_types[fieldType];			 
-	},
-	clearRegs: function() {
-		this.f_types = {};
-	},
-	createView: function(options) {
-		var f_type = options.model.get('type');
-		var cls = this.getCls(f_type);
-		var view = new cls(options);	
-
-		return view;
-	}
+function FieldViews() {
+	this.f_types = {};
 };
+
+FieldViews.prototype.reg = function (fieldType, viewCls) {
+	this.f_types[fieldType] = viewCls;
+};
+FieldViews.prototype.unreg = function (fieldType, viewCls) {
+	this.f_types[fieldType] = viewCls;
+};
+FieldViews.prototype.getCls = function(fieldType) {
+	return this.f_types[fieldType];
+};
+FieldViews.prototype.clear = function() {
+	this.f_types = {};
+};
+FieldViews.prototype.create = function(options) {
+	var f_type = options.model.get('type');
+	var cls = this.getCls(f_type);
+	var view = new cls(options);	
+
+	return view;
+};
+
+var Fields = new FieldViews();
