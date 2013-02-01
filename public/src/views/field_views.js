2f8d3bcc996bde34d87f363baff7e1e869ab5391
lvjian <lvjian@dayang.com.cn>
2013-02-01 18:05:06 +0800

    datepicker

--- public/src/views/field_views.js
+++ public/src/views/field_views.js
@@ -77,6 +77,23 @@ Fields.reg('select', SelectFieldView);
 
 
 var DateFieldView = FieldView.extend({
+	className: 'cell input-append controls bootstrap-timepicker-component',
+	template: read_tmpl('#tmpl-date'),
+	events: {
+		'changeDate input': "onChanged"
+	},
+	onRender: function() {
+		this.inputTag.datepicker({
+			showMeridian: false,
+			showSeconds: true,
+			secondStep: 30,
+			defaultTime: 'value'
+		});
+	},
+	remove: function() {
+		this.inputTag.datepicker('destroy');
+		FieldView.prototype.remove.call(this); 
+	}
 });
 
 Fields.reg('date', DateFieldView);
