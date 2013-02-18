39e201d725908dc3258194e0532d8ff8f57fe073
lvjian <lvjian@dayang.com.cn>
2013-02-18 17:09:46 +0800

    添加ColumnSpec

--- public/rsrc/main.js
+++ public/rsrc/main.js
@@ -6,17 +6,25 @@ requirejs.config({
 		"underscore": "../assets/components/underscore/underscore",
 		"backbone": "../assets/components/backbone/backbone-min",
 		"text": "../assets/components/text/text",
+
+		/* bootstrap.js & plugins*/
 		"boostrap": "../assets/components/bootstrap/js/bootstrap.min",
+		"bootstrap-date": "../assets/components/boostrap-datepicker/bootstrap-datepicker",
+		"bootstrap-time": "../assets/components/boostrap-timepicker/bootstrap-timepicker",
+		
+		/*jquery plugins*/
 		"jquery-select2": "../assets/components/select2/select2.min",
 		
+		/* source code aliase */
 		"m": "./models",
 		"v": "./views",	
 		"tmpl": "./templates",
-
+		
+		/* jasmine test */
+		"spec": "../rspec",
 		"jasmine": "../assets/components/jasmine/jasmine",
 		"jasmine-html": "../assets/components/jasmine/jasmine-html",
-		"jasmine-jquery": "../assets/components/jasmine/jasmine-jquery",
-		"spec": "../rspec"
+		"jasmine-jquery": "../assets/components/jasmine/jasmine-jquery"
 	},
 	shim: {
         'underscore': {
@@ -26,6 +34,13 @@ requirejs.config({
             deps: ['underscore', 'jquery'],
             exports: 'Backbone'
         },
+
+		'bootstrap': ['jquery'],
+		'boostrap-date': ['bootstrap'],
+		'boostrap-time': ['boostrap'],
+
+		'jquery-select2': ['jquery'],
+
 		'jasmine': {
 			exports: 'jasmine'
 		},
@@ -36,16 +51,14 @@ requirejs.config({
 		'jasmine-jquery': {
 			deps: ['jquery', 'jasmine-html'],
 			exports: 'jasmine'
-		},
-		'bootstrap': ['jquery'],
-		'jquery-select2': ['jquery']
+		}
     }
 });
 
-requirejs(['jquery', 'models/FieldModel', 'models/SelectFieldModel',
-		'views/FieldView', 'views/Fields', 'jasmine-jquery'], 
-	
-	function($, FieldModel, SelectFieldModel, FieldView, Fields, jasmine) {
+requirejs([
+	'jquery', 'jasmine-jquery',
+
+], function($, jasmine) {
 
 		var jasmineEnv = jasmine.getEnv();
 		jasmineEnv.updateInterval = 1000;
@@ -64,6 +77,7 @@ requirejs(['jquery', 'models/FieldModel', 'models/SelectFieldModel',
 		specs.push('spec/views/FieldViewSpec');
 		specs.push('spec/models/SelectFieldModelSpec');
 		specs.push('spec/views/SelectFieldViewSpec');
+		specs.push('spec/models/ColumnModelSpec');
 
 		$(function() {
 			requirejs(specs, function() {
