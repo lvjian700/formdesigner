6611ffd05748d1be73479fa99c7a97d0ddbceb1d
lvjian <lvjian@dayang.com.cn>
2013-04-17 15:30:01 +0800

    添加PlainConfig工具类，提供plain.convert方法将老版本的配置转换成新版本的

---
+++ public/rspec/models/PlainConfigSpec.js
@@ -0,0 +1,111 @@
+define([
+	'm/PlainConfig',
+	'text!tmpl/news_config.txt'
+], function(plain, config) {
+
+	describe('测试老版本新闻配置转新版json配置', function() {
+		var itemData = '0,NewsTitle,标题,0,0,100,1,1';
+		var itemJson = {
+			index: 0,
+			name: 'NewsTitle',
+			label: '标题',
+			rowIndex: 0,
+			columnIndex: 0,
+			width: 100,
+			required: true
+		};
+		
+		var fieldsData = '0,NewsTitle,标题,0,0,100,1,1;1,NewsTitle1,标题别名1,1,0,50,0,1;';
+		var numberData = '60;' + fieldsData;
+		var fieldsParts = [
+			'0,NewsTitle,标题,0,0,100,1,1',
+			'1,NewsTitle1,标题别名1,1,0,50,0,1'
+		]
+		var fieldsArray = [{
+			index: 0,
+			name: 'NewsTitle',
+			label: '标题',
+			rowIndex: 0,
+			columnIndex: 0,
+			width: 100,
+			required: true
+		}, {
+			index: 1,
+			name: 'NewsTitle1',
+			label: '标题别名1',
+			rowIndex: 1,
+			columnIndex: 0,
+			width: 50,
+			required: true
+		}];
+
+		var rowsJson = [{
+			index: 0,
+			layout: 'fit',
+			selected: false,
+			columns: [{
+				index:0,
+				width:1,
+				selected: false,
+				content: {
+					id:'NewsTitle_field',
+					name:'NewsTitle',
+					label:'标题',
+					type:'text',
+					value:'',
+					required:true
+				}
+			}]
+		}, {
+			index: 1,
+			layout: 'fit',
+			selected: false,
+			columns: [{
+				index:0,
+				width:0.5,
+				selected: false,
+				content: {
+					id:'NewsTitle1_field',
+					name:'NewsTitle1',
+					label:'标题别名1',
+					type:'text',
+					value:'',
+					required: false
+				}
+			}]
+		}];
+		
+		
+		it('parset2Item可以正确解析行数据', function() {
+			var ret = plain.parse2Item(itemData);	
+			expect(ret).toEqual(itemJson);
+		});
+
+		it('parse2Fields可以正确解析带60;开头数据,将plainText分成item段', function() {
+			var items = plain.parse2Fields(numberData); 
+
+			expect(items.length).toBe(2);
+			expect(items).toEqual(fieldsParts);
+		});
+
+		it('parse2Fields可以正确解析不带60;开头数据, 将plainText分成item段', function() {
+			var items = plain.parse2Fields(fieldsData); 
+			expect(items.length).toBe(2);
+			expect(items).toEqual(fieldsParts);
+		});
+			
+		it('fields2Json生成json rows数据', function() {
+			var ret = plain.fields2Json(fieldsParts);
+			expect(ret.length).toBe(2);
+			expect(ret).toEqual(rowsJson);
+		});
+
+		it('convert方法能将plainText配置转成rows json数据', function() {
+			var ret = plain.convert(numberData);
+			expect(ret.length).toBe(2);
+			expect(ret).toEqual(rowsJson);
+		});
+	});
+});
+
+
---
+++ public/rsrc/models/PlainConfig.js
@@ -0,0 +1,102 @@
+define([
+	'backbone', 'underscore',
+	'm/FieldModel',
+	'm/ColumnModel', 'm/ColumnCollection',
+	'm/RowModel', 'm/RowCollection',
+	'm/FormModel'
+], function(Backbone, _, FieldModel, 
+	ColumnModel, ColumnCollection, RowModel, RowCollection,
+	FormModel) {
+
+
+	function parse2Fields (plain) {
+		var parts = plain.split(';');
+		parts.pop();
+		var first = parts[0];
+
+		if(first.indexOf(',') != -1) {
+			return parts;
+		}
+
+		var fields = _.rest(parts);
+		return fields;
+	}
+
+	function parse2Item(text) {
+		var p = text.split(',');
+		var required = p[6] == '1';
+
+		return {
+			index: parseInt(p[0]),
+			name: p[1],
+			label: p[2],
+			rowIndex: parseInt(p[3]),
+			columnIndex: parseInt(p[4]),
+			width: parseInt(p[5]),
+			required: required
+		}
+	}
+
+	function fields2Json(fields) {
+		var rows = [];
+		
+		var rowIndex = -1;
+		var columnCount = 0;
+
+		for(var i = 0; i < fields.length; i++) {
+			var item = fields[i];
+			var json = parse2Item(item); 
+
+			// 新建行
+			if(rowIndex < json.rowIndex) {
+				var row = {
+					index: json.rowIndex,
+					layout: 'fit',
+					selected: false,
+					columns: []
+				}
+			
+				rowIndex = json.rowIndex;
+				rows[rowIndex] = row;
+			}
+			
+			// 创建列
+			var widthPrecent = json.width / 100.0;
+			var column = {
+				index: json.columnIndex,
+				width: widthPrecent,
+				selected: false,
+				content: {
+					id: json.name + '_field',
+					name: json.name,
+					label: json.label,
+					type: 'text',
+					value: '',
+					required: json.required
+				}
+			}
+
+			// 追加列
+			var currentRow = rows[rowIndex];
+			currentRow.columns.push(column);
+		}
+
+		return rows;
+	}
+	
+	function convert (plain) {
+		var fields = parse2Fields(plain);
+		var arrayRows = fields2Json(fields);
+		
+		return arrayRows;
+	}
+
+	return {
+		parse2Item: parse2Item,
+		parse2Fields: parse2Fields,
+		fields2Json: fields2Json,
+		convert: convert 
+	};
+});
+
+
