27e44de66a2adbd4b6e0c358bc1fdb136606bbd6
lvjian <lvjian@dayang.com.cn>
2013-04-19 14:35:30 +0800

    添加Workspace Router, 提供单元格选择的#cell/:row/:column Route

--- public/rsrc/app.js
+++ public/rsrc/app.js
@@ -1,14 +1,18 @@
 define([
 	'jquery',
+	'backbone',
 	'm/FormModel',
 	'v/FormView',
 	'./data',
-
-], function($, FormModel, FormView, config) {
+	'v/PropertyFormView'
+], function($, Backbone, FormModel, FormView, config,
+		PropertyFormView) {
 	$(function() {
 		var model = new FormModel(config);
 		var view = new FormView({model:model});
 		var el = view.render().el;
+
+		var propertyForm = new PropertyFormView();
 
 		$('#formcanvas').append(el);
 
@@ -19,6 +23,21 @@ define([
 		$('#btnRemoveRow').click(function(e) {
 			console.log('remove last row');	
 			model.removeLastRow();
+		});
+
+		var Workspace = Backbone.Router.extend({
+			routes: {
+				'cell/:row/:column': 'editCell'
+			},
+			editCell: function(row, column) {
+				console.log('editing cell');
+				var column = model.getRows().at(row)
+					.getColumns().at(column);
+				var fieldModel = column.getContent().model;	
+				console.log(fieldModel);	
+				
+
+			}
 		});
 	});
 });
