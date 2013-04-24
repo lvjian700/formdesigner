1b33fde0f26eae19815275a605b09a0225d7131a
lvjian <lvjian@dayang.com.cn>
2013-04-24 16:59:32 +0800

    添加/plain/configs/:id接口
    暂时只实现了从后台加载新闻数据
    
    返回, {content: '...'} json格式数据

--- app.rb
+++ app.rb
@@ -1,5 +1,5 @@
 require 'sinatra'
-
+require 'json'
 
 set :public_folder, File.dirname(__FILE__) + '/public'
 set :layout_engine => :erb, :layout => :layout
@@ -7,4 +7,19 @@ set :layout_engine => :erb, :layout => :layout
 
 get '/' do
     erb :index
+end
+
+get '/plain/configs/:guid' do 
+	templatePath = File.dirname(__FILE__) + '/public/rsrc/templates/news_config.txt'
+	puts templatePath
+
+	file = File.open(templatePath, 'r')
+	content = file.read
+	file.close
+	
+	ret = { :content => content }
+
+	content_type :json
+
+	ret.to_json
 end
