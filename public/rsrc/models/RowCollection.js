2365bdd7fd150fb8efddfd448ddbb074487c5ed6
lvjian700 <lvjian700@gmail.com>
2013-04-24 20:32:13 +0800

    修正comparator方法，以前的排序使用是错误的

--- public/rsrc/models/RowCollection.js
+++ public/rsrc/models/RowCollection.js
@@ -5,9 +5,15 @@ define([
 	
 	var RowCollection = Backbone.Collection.extend({
 		model: RowModel,
-		comparator: function() {
-			return this.get('index');
-		}
+        comparator: function(now, next) {
+            var nowIndex = now.get('index');
+            var nextIndex = now.get('index');
+
+            if (nowIndex > nextIndex) return 1;
+            if (nowIndex < nextIndex) return -1;
+
+            return 0;
+        }
 	});
 
 	return RowCollection;
