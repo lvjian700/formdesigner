8c2d70ea9e3a954c9d439957c80d29b45e9e0648
lvjian700 <lvjian700@gmail.com>
2013-05-09 08:53:08 +0800

    systemConfigs 新增configType 配置，提供数值转换

--- public/rsrc/utils/config.js
+++ public/rsrc/utils/config.js
@@ -2,9 +2,15 @@ var Configs = {
 	systemConfig: {
 		save: '/configs/save.action',
 		get: '/configs/get.action',
-		list: '/configs/list.action'
+		list: '/configs/list.action',
+        configType: {
+            10: '文稿',
+            12: '选题'
+        }
 	},
 	canvas: {
 		width: 1280
 	}
 };
+
+var SC = Configs.systemConfig;
