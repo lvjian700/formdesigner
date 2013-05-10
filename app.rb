0f5879c7cf362bf50d83526cdc34bc1caf0def0e
lvjian700 <lvjian700@gmail.com>
2013-05-10 23:42:05 +0800

    ruby下模拟环境
    提供如下接口
    
    /configs/get.action
    
    /configs/list.action

--- app.rb
+++ app.rb
@@ -1,15 +1,23 @@
+#encoding=utf-8
 require 'sinatra'
 require 'json'
 
 set :public_folder, File.dirname(__FILE__) + '/public'
 set :layout_engine => :erb, :layout => :layout
 
+templatePath = File.dirname(__FILE__) + '/public/rsrc/templates/news_config.txt'
+file = File.open(templatePath, 'r')
+content = file.read
+file.close
+
+config = {:configGuid => '1111111111111111111111', :configName => "新闻配置", :configValue => content, :orderNum => 4, :preceptName => "测试组www"}
+json_config = config.to_json
 
 get '/' do
     erb :index
 end
 
-post '/plain/configs/:guid' do 
+post '/configs/post.action' do 
 	guid = params[:guid]
 	puts "url parameter :guid = #{guid}"
 	content= params[:content]
@@ -19,17 +27,14 @@ post '/plain/configs/:guid' do
 	'success'
 end
 
-get '/plain/configs/:guid' do 
-	templatePath = File.dirname(__FILE__) + '/public/rsrc/templates/news_config.txt'
-	puts templatePath
+get '/configs/list.action' do 
+    "[#{json_config}]"
+end
 
-	file = File.open(templatePath, 'r')
-	content = file.read
-	file.close
-	
-	ret = { :content => content }
+get '/configs/get.action' do 
+    puts "get config by id: #{params[:guid]}"
+  guid = params[:guid]
 
-	content_type :json
-
-	ret.to_json
+  content_type :json
+  "{\"body\": #{json_config } }"
 end
